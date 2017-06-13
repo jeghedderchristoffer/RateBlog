118c2628701075267c28c67794b6a9a676201570
Rasmus <Rasmus-nohr@hotmail.com>
2017-06-13 12:54:13 +0200

    Migration Rating

--- RateBlog/Data/Migrations/20170613105206_Rating.Designer.cs
+++ RateBlog/Data/Migrations/20170613105206_Rating.Designer.cs
@@ -8,9 +8,10 @@ using RateBlog.Data;
 namespace RateBlog.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170613105206_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
