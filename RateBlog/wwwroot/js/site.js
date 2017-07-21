af272088c35902108a6807147c2a0798fc1bf9c4
Rasmus <Rasmus-nohr@hotmail.com>
2017-07-21 15:07:14 +0200

    Footer + søg updated

--- RateBlog/wwwroot/js/site.js
+++ RateBlog/wwwroot/js/site.js
@@ -325,3 +325,25 @@ $(document).ready(function () {
         }
     });
 });
+
+
+
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+
+
+
