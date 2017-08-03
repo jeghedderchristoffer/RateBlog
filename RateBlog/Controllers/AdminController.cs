c17d3a06039b90322a81ae04eb085f41636d5724
Miki Møller <itsaduck55@gmail.com>
2017-08-03 08:53:56 +0200

    Adminside start op side

---
+++ RateBlog/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RateBlog.Data;
+using RateBlog.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RateBlog.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        private readonly ApplicationDbContext _context;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminController (ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            var model =_userManager.Users.ToList();
+
+            return View(model);
+        }
+
+    }
+}
